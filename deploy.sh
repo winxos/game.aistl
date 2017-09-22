5812f4adcb19fa1775f548347f33e011a23755a3
winxos <winxos@hotmail.com>
2017-09-22 10:18:36 +0800

    add git reset

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ date >> "$LOG_FILE"
 echo "Start deployment" >>"$LOG_FILE"
 cd /home/wvv/web/game.aistl/
 echo "pulling source code..." >> "$LOG_FILE"
+git reset --hard
 git pull origin master
 echo "Finished." >>"$LOG_FILE"
 echo >> $LOG_FILE
