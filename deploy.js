d60a2d9d7b237d46c37f59762b9039980caf56f2
winxos <winxos@hotmail.com>
2017-09-22 10:12:01 +0800

    modify port modify log path

--- deploy.js
+++ deploy.js
@@ -1,31 +1,38 @@
 var http = require('http');
 var createHandler = require('github-webhook-handler');
-var handler = createHandler({ path: '/', secret: '112211' });
+var handler = createHandler({path: '/', secret: '112211'});
+
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
 function run_cmd(cmd, args, callback) {
-  var spawn = require('child_process').spawn;
-  var child = spawn(cmd, args);
-  var resp = "";
+    var spawn = require('child_process').spawn;
+    var child = spawn(cmd, args);
+    var resp = "";
 
-  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
-  child.stdout.on('end', function() { callback (resp) });
+    child.stdout.on('data', function (buffer) {
+        resp += buffer.toString();
+    });
+    child.stdout.on('end', function () {
+        callback(resp)
+    });
 }
 
 http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404;
-    res.end('no such location')
-  })
-}).listen(777);
+    handler(req, res, function (err) {
+        res.statusCode = 404;
+        res.end('no such location')
+    })
+}).listen(12345);
 
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
+    console.error('Error:', err.message)
 });
 
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref);
-    run_cmd('sh', ['./deploy.sh',event.payload.repository.name], function(text){ console.log(text) });
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref);
+    run_cmd('sh', ['./deploy.sh', event.payload.repository.name], function (text) {
+        console.log(text)
+    });
 });
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-LOG_FILE="/var/log/deploy.log"
+LOG_FILE="game.log"
 
 date >> "$LOG_FILE"
 echo "Start deployment" >>"$LOG_FILE"
 cd /home/wvv/web/game.aistl/
 echo "pulling source code..." >> "$LOG_FILE"
-git checkout origin master
 git pull origin master
 echo "Finished." >>"$LOG_FILE"
 echo >> $LOG_FILE
